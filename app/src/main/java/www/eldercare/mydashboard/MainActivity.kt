ab5ace4562e0bf6e6ca158275165024e25a0e0df
aydemiralp <alp.aydemir1@gmail.com>
2021-07-13 12:56:21 +0300

    elder-care app

---
+++ app/src/main/java/www/eldercare/mydashboard/MainActivity.kt
@@ -0,0 +1,14 @@
+package www.eldercare.mydashboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import www.eldercare.mydashboard.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
